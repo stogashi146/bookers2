4c9a170b678f6acce752947e90d0e96511533902
EC2 Default User <ec2-user@ip-172-31-21-143.ec2.internal>
2021-06-11 05:19:24 +0000

    [Finish] 投稿フォーム、一覧ページ、詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # top.html.erbをトップページに設定
   root to:"homes#top"
+  resources :books,only:[:new,:create,:index,:show,:destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_025503) do
+ActiveRecord::Schema.define(version: 2021_06_11_031607) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_025503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
