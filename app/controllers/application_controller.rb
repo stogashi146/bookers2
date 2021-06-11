8d154230fd7984881cb42640e796237a9f3cec25
EC2 Default User <ec2-user@ip-172-31-21-143.ec2.internal>
2021-06-11 03:00:36 +0000

    [Finish] topページ ユーザ登録認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # device利用の機能が使用される場合、:configure_permitted~が実行される
+  before_action :configure_permitted_parameters,if: :devise_controller?
+  # 他のコントローラからも参照できる
+  protected
+  # signupの際に、nameのデータ操作が許可される
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # top.html.erbをトップページに設定
+  root to:"homes#top"
 end
