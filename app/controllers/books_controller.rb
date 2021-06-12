42cb42c0f6095fcad9f6f5fe8fd074870f2b8852
EC2 Default User <ec2-user@ip-172-31-21-143.ec2.internal>
2021-06-12 08:18:09 +0000

    [Start] BootStrap実装開始

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -53,9 +53,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
-    flash[:success]="You have updated book successfully."
+    if @book.update(book_params)
+        redirect_to book_path(@book.id)
+        flash[:success]="You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
