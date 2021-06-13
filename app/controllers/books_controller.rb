f29d42c6eb1e5698dfc1a819f75fdbb22b646333
EC2 Default User <ec2-user@ip-172-31-21-143.ec2.internal>
2021-06-13 11:37:18 +0000

    [Start] Rspecテスト開始

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,6 +43,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     # テンプレート化するために、@userに@book.userを格納
     @user = @book.user
+    # show内（book詳細）に投稿を置く場合、newが必要
+    @book_new = Book.new
   end
 
   def edit
